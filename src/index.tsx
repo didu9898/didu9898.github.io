418ddfe98c5f56a45e79b77aa60d7ffa70aa1ced
Drashti Patel <drashti.patel@infor.com>
2025-04-30 14:06:02 +0200

    Keyboard shortcut bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+window.onload = async () => {
+  if (typeof Office !== "undefined") {
+    Office.onReady((info) => {
+      if (info.host === Office.HostType.Excel) {
+        Office.actions.associate(
+          "showTPWorkbook",
+          async (event: Office.AddinCommands.Event) => {
+            await Office.addin.showAsTaskpane();
+            event.completed();
+          }
+        );
+      }
+    });
+  }
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+  );
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
